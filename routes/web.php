be4e11ecbd19a1f597dfac1ddefcf157336cb27d
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2024-02-17 09:32:22 +0600

    laravel mark down

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $post = \App\Models\Post::first();
+
+    return view('welcome',compact('post'));
 });
